1c29844b6730c1c04d93b4c1ad445dc732eb696d
Yarden <77342408+yardenfalik@users.noreply.github.com>
2024-02-26 19:43:47 +0000

    upgrading the website

--- app.js
+++ app.js
@@ -25,3 +25,15 @@ function setColors()
     }
 }
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((element) => observer.observe(element));
