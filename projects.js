3ff1d5e02f47db2eba62cd4b5790b3d89de9f8c7
Yarden <77342408+yardenfalik@users.noreply.github.com>
2024-12-08 20:09:55 +0000

    fixing

--- projects.js
+++ projects.js
@@ -2,25 +2,38 @@ function setGradientBackground(color1, color2)
 {
     var bg = document.getElementsByClassName("customBackground")[0];
 
-    bg.style.opacity = '1';
-    //document.body.style.background = `linear-gradient(to right, ${color1}, ${color2})`;
+    bg.style.transition = 'background 2s ease-in-out';
+
+    bg.animate([
+        // key frames
+        { opacity: bg.style.opacity },
+        { opacity: 0.5 }
+      ], {
+        // sync options
+        duration: 1000,
+        iterations: 1
+    }); 
+
     bg.style.background = `
     radial-gradient(circle at 30% 40%, ${color1}, transparent 80%), 
     radial-gradient(circle at 70% 60%, ${color2}, transparent 80%)`;
-
-    bg.style.transition = 'background 0.5s ease';
-    document.body.style.color = '#1D1D1F';
+    
+    bg.style.opacity = 0.5;
 }
 
 function resetBackground() 
 {
     var bg = document.getElementsByClassName("customBackground")[0];
-    bg.style.opacity = '0';
 
-    bg.style.background = '';
+    bg.animate([
+        // key frames
+        { opacity: 0.5 },
+        { opacity: 0 }
+      ], {
+        // sync options
+        duration: 1000,
+        iterations: 1
+    });
 
-    document.body.style.transition = 'background 0.5s ease, color 0.5s ease'; 
-    document.body.style.transition = 'all 0.5s ease'; 
-
-    document.body.style.color = '';
+    bg.style.opacity = 0;
 }
