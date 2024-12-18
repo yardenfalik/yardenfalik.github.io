f217918b32a7caf4323c3630e0950104e7245b32
Yarden <77342408+yardenfalik@users.noreply.github.com>
2024-12-18 21:24:12 +0000

    adding theme menu for changing theme

--- theme.js
+++ theme.js
@@ -1,24 +1,65 @@
-function lightMode() 
-{
-    themeButton = document.getElementById('themeButton');
-
-    if (document.body.className === 'lightMode') 
-    {
-        themeButton.innerHTML = '<img src="img/moonIcon.png" class="moonIcon">';
-        document.body.className = 'darkMode';
-        return;
-    }
-
-    themeButton.innerHTML = '<img src="img/sunIcon.png" class="sunIcon">';
+function lightMode()
+{  
+    document.getElementById('themeButton').innerHTML = '<img src="img/sunIcon.png" class="sunIcon">';
     document.body.className = 'lightMode';
+    
+    saveTheme("light");
 }
 
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) 
+function darkMode()
 {
-    themeButton.innerHTML = '<img src="img/moonIcon.png" class="moonIcon">';
+    document.getElementById('themeButton').innerHTML = '<img src="img/moonIcon.png" class="moonIcon">';
     document.body.className = 'darkMode';
+
+    saveTheme("dark");
 }
-else {
-    themeButton.innerHTML = '<img src="img/sunIcon.png" class="sunIcon">';
-    document.body.className = 'lightMode';
+
+function system()
+{
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) 
+    {
+        document.getElementById('themeButton').innerHTML = '<img src="img/moonIcon.png" class="moonIcon">';
+        document.body.className = 'darkMode';
+    }
+    else
+    {
+        document.getElementById('themeButton').innerHTML = '<img src="img/sunIcon.png" class="sunIcon">';
+        document.body.className = 'lightMode';
+    }
+    saveTheme("system");
 }
+
+function saveTheme(theme)
+{
+    localStorage.setItem("theme", theme);
+    ["dark", "light", "system"].forEach(id => 
+        document.getElementById(id).textContent = "‎ ‎ " + id.charAt(0).toUpperCase() + id.slice(1)
+    );
+    document.getElementById(theme).textContent += " •";
+}
+
+function themeMenu()
+{
+    var menu = document.getElementById("themeMenu");
+    menu.style.display = (menu.style.display == "none") ? "grid" : "none";
+}
+
+function getTheme()
+{
+    var theme = localStorage.getItem("theme");
+
+    if (theme === "dark")
+    {
+        darkMode();
+    }
+    else if (theme === "light")
+    {
+        lightMode();
+    }
+    else
+    {
+        system();
+    }
+}
+
+getTheme();
